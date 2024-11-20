ef94b30c9d9e884d2c7ef121f6013ae13ab8f426
haruband <haruband@gmail.com>
2024-11-20 15:46:16 +0900

    Fix datafusion.join.benchmark blog

--- datafusion/join.benchmark/plot.py
+++ datafusion/join.benchmark/plot.py
@@ -5,23 +5,35 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv(sys.argv[1])
 
+plt.figure(figsize=(10, 8))
+
 axe1 = plt.subplot()
 axe2 = axe1.twinx()
 
-g1 = sns.barplot(ax=axe1, data=df, x="target", y="runtime", palette="pastel")
-g1.set_xticklabels(g1.get_xticklabels(), rotation=45, size=7)
+g1 = sns.barplot(
+    data=df,
+    x="target",
+    y="runtime",
+    palette="pastel",
+    ax=axe1,
+)
+g1.set_xticklabels(g1.get_xticklabels(), rotation=10, size=8)
+g1.set_yticklabels(g1.get_yticklabels(), size=8)
 g1.set(xlabel=None)
+g1.set(ylabel="runtime (seconds)")
 
 g2 = sns.lineplot(
-    ax=axe2,
     data=df,
     x="target",
     y="memory",
     marker="o",
     linestyle="dashed",
     color="green",
+    ax=axe2,
 )
+g2.set_yticklabels(g2.get_yticklabels(), size=8)
+g2.set(ylabel="memory (gigabytes)")
 
-plt.xticks(rotation=45)
 plt.tight_layout()
-plt.show()
+# plt.show()
+plt.savefig("tpch.png")
